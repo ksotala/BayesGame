e1c189539728a5dac9cf53bb0ab938c79ffe0b2b
Kaj Sotala <xuenay@gmail.com>
2015-09-20 20:06:03 +0300

    Doing changes to BayesPainter

--- BayesGame/src/bayesGame/ui/painter/BayesPainter.java
+++ BayesGame/src/bayesGame/ui/painter/BayesPainter.java
@@ -23,20 +23,10 @@ public class BayesPainter {
 		Graphics g = img.getGraphics();
 		
 		NodePainter.graphicBackgroundPainter(g, 0, 0, size_x, (int)(1.5*size_y));
-	
-		double cells = rows * columns;
-		double coloredCells = percentage * cells;
-		
+			
 		Color greyScaleTrueColor = Color.GRAY;
 		Color greyScaleFalseColor = Color.BLACK;
-		
-		
-
-		int rows1 = processProbability(parentNodeProbability);
-		int rows2 = processProbability(Fraction.ONE.subtract(parentNodeProbability));
-		
-		
-		
+				
 		String CPTDescription = node.cptDescription;
 		int trueFirstDigit = 0;
 		while (!Character.isDigit(CPTDescription.charAt(trueFirstDigit))) trueFirstDigit++;
@@ -68,8 +58,24 @@ public class BayesPainter {
 			falseBarColor = Color.WHITE;
 		}
 		
-		for (int i = 0; i < rows1; i++){
+		// int rows1 = processProbability(parentNodeProbability);
+		// int rows2 = processProbability(Fraction.ONE.subtract(parentNodeProbability));
+		
+		double rows1 = (parentNodeProbability.doubleValue() / 0.2);
+		double rows2 = ((Fraction.ONE.subtract(parentNodeProbability)).doubleValue() / 0.2);
+
+		int rows1_asint = (int)rows1;
+		int rows2_asint = (int)rows2;
+		
+		double rows1_remainder = rows1 - rows1_asint;
+		double rows2_remainder = rows2 - rows2_asint;
+		
+		for (int i = 0; i < (rows1-1); i++){
 			paintProbabilityGrid(g, (columns * trueProbability), trueBarColor, falseBarColor, 0, (cell_size + 2) + (i * cell_size), 1, columns, (int)(cell_size * 0.5), cell_size);			
+		}
+		
+		if (rows1_remainder > 0){
+			paintProbabilityGrid(g, (columns * trueProbability * rows1_remainder), trueBarColor, falseBarColor, 0, (cell_size + 2) + ((int)rows1 * cell_size), rows1_remainder, columns, (int)(cell_size * 0.5), (int)(cell_size*rows1_remainder));
 		}
 		
 		for (int i = 0; i < rows2; i++){
@@ -84,40 +90,19 @@ public class BayesPainter {
 		return img;
 	}
 
-	
-	private static int processProbability(Fraction probability) {
-		int rows = 0;
-		double probabilityDouble = probability.doubleValue();
-		
-		if (probabilityDouble > 0){
-			rows++;
-		}
-		if (probabilityDouble > 0.2){
-			rows++;
-		}
-		if (probabilityDouble > 0.4){
-			rows++;
-		}
-		if (probabilityDouble > 0.6){
-			rows++;
-		}
-		if (probabilityDouble > 0.8){
-			rows++;
-		}
-		
-		return rows;
-	}
 
+	public static void paintProbabilityGrid(Graphics g, double coloredCells, Color gridColor, Color falseColor, int x0, int y0, double rows, int columns, int cell_size_x, int cell_size_y){
 
-	public static void paintProbabilityGrid(Graphics g, double coloredCells, Color gridColor, Color falseColor, int x0, int y0, int rows, int columns, int cell_size_x, int cell_size_y){
-		int cells = rows * columns;
+		double rows_original = rows;
+		
+		double cells = rows * columns;
 		
 		if (cells < coloredCells){
 			throw(new IllegalArgumentException("Requested " + coloredCells + " cells when only " + cells + " cells available"));
 		}
 		
-		int size_x = cell_size_x * columns;
-		int size_y = cell_size_y * rows;
+		int size_x = (int)(cell_size_x * columns);
+		int size_y = (int)(cell_size_y * rows);
 		
 		int x = 0;
 		int y = 0;
@@ -129,6 +114,8 @@ public class BayesPainter {
 		double coloredCellsIntegral = coloredCells % 1;
 		
 		g.setColor(gridColor);
+		
+		rows = (int)rows;
 		
 		for (int i=0; i < coloredCellsFractional; i++){			
 			int cellX = x0 + (x * cell_size_x);
@@ -145,8 +132,8 @@ public class BayesPainter {
             	int roundedFractionalCellSize = (int)Math.round(fractionalCellSize);
             	g.fillRect(cellX, cellY, roundedFractionalCellSize, cell_size_y);
             }
-            
-			if (y == (rows - 1)){
+
+            if (y == (rows - 1)){
 				y = 0;
 				x++;
 			} else {
@@ -154,21 +141,15 @@ public class BayesPainter {
 			}
 		}
 		
-		// draw the borders between individual boxes
-/*
-		g.setColor(Color.GRAY);
-        for (int i = 0; i <= size_x; i += cell_size_x) {
-            g.drawLine(i + x0, 0 + y0, i + x0, size_y + y0);
-        }
+		g.setColor(Color.BLACK);
+		
+		if (rows_original % 1 == 0){
+	        g.drawRect(x0, y0, size_x, size_y);
+		} else {
+			g.drawLine(x0 + size_x, y0, 0, size_y);
+		}
 
-        for (int i = 0; i <= size_y; i += cell_size_y) {
-            g.drawLine(0 + x0, i + y0, size_x + x0, i + y0);
-        }
-        
-        */
 			
-        g.setColor(Color.BLACK);
-        g.drawRect(x0, y0, size_x, size_y);
 	}
 	
 	
